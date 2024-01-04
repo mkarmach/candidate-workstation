049e3f5ff01a93310fae6d25580590f9715e20c2
mkarmach <155649879+mkarmach@users.noreply.github.com>
2024-01-04 16:37:23 +0100

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Cisco Certified DevNet Expert (v1.0) Equipment and Software List'
+copyright = '2024, Manoj Karmacharya'
+author = 'Manoj Karmacharya'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx-rtd-theme'
+html_static_path = ['_static']
